e7c4506ba020e452446fb02c16ca3889004b347b
Uwe <bang@metalfests.eu>
2025-09-24 13:03:05 +0000

    Fix an architectural misunderstanding and add the service calls at the right spot

--- homeassistant/components/music_favorites/__init__.py
+++ homeassistant/components/music_favorites/__init__.py
@@ -60,31 +60,8 @@ async def _init_flow(hass: HomeAssistant) -> None:
 
 
 async def async_setup(hass: HomeAssistant, _config: ConfigType) -> bool:
-    """Set up Music Favorites from YAML."""
-    _LOGGER.debug("Setting up Music Favorites from configuration.yaml")
-
-
-    # We are not registered in the Integrations list, hence we can only load via
-    # configuration.yaml.
-    # When this loads we create an instance of the service - but only ONCE, hence
-    # the conditional
-    existing_entries = hass.config_entries.async_entries(DOMAIN)
-    if not existing_entries:
-        _LOGGER.debug(
-            "Creating initial Music Favorites Service to be managed from UI from there on"
-        )
-        # We're artificially running the Config Flow here once
-        hass.async_create_task(_init_flow(hass))
-
-    return True
-
-
-async def async_setup_entry(
-    hass: HomeAssistant, entry: MusicFavoritesConfigEntry
-) -> bool:
-    """Set up Music Favorites from a config entry."""
-
-    _LOGGER.debug("Setting up config entry: %s", entry.title)
+    """Set up Music Favorites."""
+    _LOGGER.debug("Setting up Music Favorites")
 
     async def add_favorite_service(call: ServiceCall) -> None:
         """Add a new favorite to the collection."""
@@ -93,7 +70,9 @@ async def async_setup_entry(
         if config_entry_id:
             target_entry = hass.config_entries.async_get_entry(config_entry_id)
             if not target_entry:
-                raise ServiceValidationError(f"Config entry {config_entry_id} not found")
+                raise ServiceValidationError(
+                    f"Config entry {config_entry_id} not found"
+                )
         else:
             # For voice commands, auto-find the music_favorites entry
             entries = hass.config_entries.async_entries(DOMAIN)
@@ -115,9 +94,32 @@ async def async_setup_entry(
         )
 
     # Register the service
+    _LOGGER.debug("Registering add_favorite service")
     hass.services.async_register(
         DOMAIN, "add_favorite", add_favorite_service, schema=ADD_FAVORITE_SCHEMA
     )
+
+    # We are not registered in the Integrations list, hence we can only load via
+    # configuration.yaml.
+    # When this loads we create an instance of the service - but only ONCE, hence
+    # the conditional
+    existing_entries = hass.config_entries.async_entries(DOMAIN)
+    if not existing_entries:
+        _LOGGER.debug(
+            "Creating initial Music Favorites Service to be managed from UI from there on"
+        )
+        # We're artificially running the Config Flow here once
+        hass.async_create_task(_init_flow(hass))
+
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: MusicFavoritesConfigEntry
+) -> bool:
+    """Set up a Music Favorites Config Entry."""
+
+    _LOGGER.debug("Setting up config entry: %s", entry.title)
 
     await hass.config_entries.async_forward_entry_setups(entry, _PLATFORMS)
 
